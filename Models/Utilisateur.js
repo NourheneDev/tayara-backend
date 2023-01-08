0000bf16358e18d0662805f0c70f19b436751a7e
NourheneDev <nourhene.developement@gmail.com>
2023-01-08 11:29:58 +0100

    test user model

--- Models/Utilisateur.js
+++ Models/Utilisateur.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema({
     email : {type : String , required : true , unique : true },
     password : String,
-    nom : String,
+    nom_user : String,
     prenom : String,
     image : String ,
-    adresse : String
+    adresse : String,
+    test : Number
 })
 
 module.exports = mongoose.model('Utilisateur' , schema)
